9216a8052e0d0476088bd071f2f034f2891a6ddc
AdelAliSalehSallam <adel.ali.sallam@gmail.com>
2026-07-23 20:49:17 +0300

    Trust Railway proxies for HTTPS admin login

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -14,6 +14,7 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware): void {
+        $middleware->trustProxies(at: '*');
         $middleware->redirectGuestsTo(fn () => route('admin.login'));
         $middleware->alias([
             'token.auth' => \App\Http\Middleware\AuthenticateAccessToken::class,
